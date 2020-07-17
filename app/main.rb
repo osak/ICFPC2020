1ae45a64bd9e040af16fa649a702db742fa06428
Osamu Koga <osak.63@gmail.com>
2020-07-17 01:38:21 +0000

    APIの仕様が変わったっぽい

--- app/main.rb
+++ app/main.rb
@@ -1,13 +1,27 @@
 require 'net/http'
 
 def main
-  serverurl = ARGV[0]
-  playerkey = ARGV[1]
-  puts "ServerUrl: %s; PlayerKey: %s" % [serverurl, playerkey]
+  begin
+    serverurl = ARGV[0]
+    playerkey = ARGV[1]
+    puts "ServerUrl: %s; PlayerKey: %s" % [serverurl, playerkey]
 
-  uri = URI(serverurl + "?playerKey=#{playerkey}")
-  res = Net::HTTP.get_response(uri)
-  puts res
+    uri = URI(serverurl)
+    res = Net::HTTP.post(uri, playerkey)
+    if res.code == "200"
+      puts "Server response: %s" % res.body
+    else
+      puts "Unexpected server response:"
+      puts "HTTP code: %s" % res.code
+      puts "Response body: %s" % res.body
+      exit(2)
+    end
+  rescue StandardError => e
+    puts "Unexpected server response:"
+    puts e
+    puts e.backtrace
+    exit(1)
+  end
 end
 
 if __FILE__ == $0
