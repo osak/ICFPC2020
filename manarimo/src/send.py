4fd92aced44726c08ebeba6e94d76409cdb50848
amylase <amylase.inquiry@gmail.com>
2020-07-18 04:05:44 +0900

    fixed a bug

--- manarimo/src/send.py
+++ manarimo/src/send.py
@@ -7,19 +7,19 @@ API_BASE = "https://icfpc2020-api.testkontur.ru"
 API_KEY = "decffdda9f2d431792a37fbfb770f825"
 
 
-def exchange(raw_request):
-    request = mod.run(raw_request)
+def exchange(request):
+    raw_request = mod.run(request)
     params = {
         "apiKey": API_KEY
     }
-    post_result = requests.post(API_BASE + "/aliens/send", data=request, params=params)
+    post_result = requests.post(API_BASE + "/aliens/send", data=raw_request, params=params)
     if post_result.status_code != 200:
         raise IOError("Failed to communicate with the server")
     raw_response = post_result.text
     response = dem.run(raw_response)
     return {
-        "raw_request": raw_request,
         "request": request,
+        "raw_request": raw_request,
         "raw_response": raw_response,
         "response": response
     }
