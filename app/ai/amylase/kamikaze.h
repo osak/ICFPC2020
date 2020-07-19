25fe0b250bc3c80cb2478f8061060b92357ebe00
amylase <amylase.inquiry@gmail.com>
2020-07-20 05:11:52 +0900

    令和の神風

--- app/ai/amylase/kamikaze.h
+++ app/ai/amylase/kamikaze.h
@@ -5,7 +5,8 @@
 #include "../../game.h"
 #include "../../ai.h"
 
-class MeteorAI : public AI {
+class KamikazeAI : public AI {
+    const int kamikaze_rad = 3;
 
     bool dead(const Vector &loc, int center_rad, int field_rad) {
         if (abs(loc.x) <= center_rad && abs(loc.y) <= center_rad) return true;
@@ -29,19 +30,25 @@ class MeteorAI : public AI {
         return make_pair(loc, vel);
     };
 
-    int test(Vector loc, Vector vel, int center_rad, long long field_rad, int max_turn, bool show_trail = false) {
-        int cnt = 0;
+    int test(Vector loc, Vector vel, Vector eloc, Vector evel, int center_rad, long long field_rad, int max_turn, int offset, bool show_trail = false) {
+        int cnt = offset;
+        const int trace_turn = 10;
 
         while (cnt < max_turn) {
             auto p = simulate(loc, vel);
+            auto q = simulate(eloc, evel);
             loc = p.first;
             vel = p.second;
-
+            eloc = q.first;
+            evel = q.second;
             if (show_trail) {
                 cerr << loc << endl;
             }
 
             if (dead(loc, center_rad, field_rad)) break;
+            if (cnt <= trace_turn && max(abs(loc.x - eloc.x), abs(loc.y - eloc.y)) <= kamikaze_rad) {
+                return max_turn + (trace_turn + 1 - cnt);
+            }
             ++cnt;
         }
 
@@ -56,11 +63,15 @@ class MeteorAI : public AI {
         return a.x == b.x || a.y == b.y || (abs(a.x - b.x) == abs(a.y - b.y));
     }
 
-    pair<int, Vector> dfs(int depth, const Vector &loc, const Vector &vel, const vector<Vector> &vec, int center_rad, long long field_rad, int max_turn, bool danger) {
+    pair<int, Vector> dfs(int depth, const Vector &loc, const Vector &vel, const Vector &eloc, const Vector &evel, const vector<Vector> &vec, int center_rad, long long field_rad, int max_turn, bool danger) {
         if (depth == 4) return make_pair(-1, Vector(0, 0));
 
         int best = -1;
         Vector best_move = Vector(0, 0);
+
+        const auto next_enemy = simulate(eloc, evel);
+        const Vector next_eloc = next_enemy.first;
+        const Vector next_evel = next_enemy.second;
 
         int use_thruster = danger && depth == 0;
         for (int i = 0 + use_thruster; i < vec.size() + use_thruster; ++i) {
@@ -70,7 +81,7 @@ class MeteorAI : public AI {
             new_vel.x += v.x;
             new_vel.y += v.y;
 
-            int result = test(loc, new_vel, center_rad, field_rad, max_turn);
+            int result = test(loc, new_vel, eloc, evel, center_rad, field_rad, max_turn, depth);
 
             auto next_param = simulate(loc, new_vel);
             if (dead(next_param.first, center_rad, field_rad)) continue;
@@ -81,7 +92,7 @@ class MeteorAI : public AI {
             }
 
 
-            auto dfs_result = dfs(depth + 1, next_param.first, next_param.second, vec, center_rad, field_rad, max_turn, false);
+            auto dfs_result = dfs(depth + 1, next_param.first, next_param.second, next_eloc, next_evel, vec, center_rad, field_rad, max_turn, false);
             if (best < dfs_result.first) {
                 best = dfs_result.first;
                 best_move = v;
@@ -91,7 +102,7 @@ class MeteorAI : public AI {
         return make_pair(best, best_move);
     }
 
-    Vector safe_move(long long planet_size, int field_size, const Vector &loc, const Vector &vel, int remaining_turn, bool danger) {
+    Vector safe_move(long long planet_size, int field_size, const Vector &loc, const Vector &vel, const Vector &eloc, const Vector &evel, int remaining_turn, bool danger) {
         vector<vector<Vector>> vecs = {
                 {Vector(0, 0), Vector(0, 1),  Vector(1, 0),  Vector(1, 1)},
                 {Vector(0, 0), Vector(0, 1),  Vector(-1, 0), Vector(-1, 1)},
@@ -102,7 +113,7 @@ class MeteorAI : public AI {
         int best = -1;
         Vector best_move = Vector(0, 0);
         for (const auto &vec: vecs) {
-            auto result = dfs(0, loc, vel, vec, planet_size, field_size, remaining_turn, danger);
+            auto result = dfs(0, loc, vel, eloc, evel, vec, planet_size, field_size, remaining_turn, danger);
 
             auto new_vel = vel;
             new_vel.x += result.second.x; new_vel.y += result.second.y;
@@ -119,9 +130,24 @@ class MeteorAI : public AI {
         return Vector(-best_move.x, -best_move.y);
     }
 
+    bool check_kamikaze(
+            const Vector &loc, const Vector &vel,
+            const Vector &enemy_loc, const Vector &enemy_vel
+    ) {
+        const int next_my_x = loc.x + vel.x;
+        const int next_my_y = loc.y + vel.y;
+        const int next_op_x = enemy_loc.x + enemy_vel.x;
+        const int next_op_y = enemy_loc.y + enemy_vel.y;
+
+        const int distance = max(abs(next_my_x - next_op_x), abs(next_my_y - next_op_y));
+        return distance <= kamikaze_rad;
+    }
+
+
 public:
     void test_safe_move(int x, int y) {
         Vector loc(x, y), vel(0, 0);
+        Vector eloc(-x, -y), evel(0, 0);
 
         int planet = 16;
         int field_rad = 128;
@@ -133,7 +159,11 @@ public:
         int cnt = 0;
         while(cnt < 256) {
             // simulate
-            auto vec = safe_move(planet, field_rad, loc, vel, 256, false);
+            if (check_kamikaze(loc, vel, eloc, evel)) {
+                cerr << "kamikaze!" << endl;
+            }
+
+            auto vec = safe_move(planet, field_rad, loc, vel, eloc, evel, 256, false);
 
             if (vec.x != 0 || vec.y != 0) {
                 ++cost;
@@ -149,16 +179,16 @@ public:
 
             ++cnt;
 
-//            cerr << "current loc:" << loc << " current vel" << vel << endl;
+            cerr << "current loc:" << loc << " current vel" << vel << endl;
         }
 
         if (cnt > 256 && cost < 30) {
 //            cerr << "cost" << cost << " loc:" << loc << " vel:" << vel << " " << test(loc, vel, planet, field_rad, 256, false) << endl;
         } else {
             if (cnt < 250) {
-                cout << "dead at turn cnt: " << cnt << " cost" << cost << " loc:" << loc << " vel:" << vel << " " << test(loc, vel, planet, field_rad, 256, false) << endl;
+                cout << "dead at turn cnt: " << cnt << " cost" << cost << " loc:" << loc << " vel:" << vel << " " << test(loc, vel, eloc, evel, planet, field_rad, 256, 0, false) << endl;
             } else {
-                cout << "cost" << cost << " loc:" << loc << " vel:" << vel << " " << test(loc, vel, planet, field_rad, 256, false) << endl;
+                cout << "cost" << cost << " loc:" << loc << " vel:" << vel << " " << test(loc, vel, eloc, evel, planet, field_rad, 256, 0, false) << endl;
             }
         }
     }
@@ -184,7 +214,16 @@ public:
         auto evel = !response.game_info.is_defender ? response.game_state.defender_states[0].velocity : response.game_state.attacker_states[0].velocity;
 
         Vector my_location(pos.first, pos.second), my_velocity(vel.first, vel.second);
-        Vector next_move = safe_move(response.game_info.field_info.planet_radius, response.game_info.field_info.field_radius, my_location, my_velocity, response.game_info.max_turns - response.game_state.current_turn,
+        Vector enemy_location(epos.first, epos.second), enemy_velocity(evel.first, evel.second);
+
+        if (check_kamikaze(my_location, my_velocity, enemy_location, enemy_velocity)) {
+            cout << "Ten-nou Heika Banzai!!!!!" << endl;
+            CommandParams params;
+            params.commands.push_back(new Kamikaze(unit_id));
+            return params;
+        }
+
+        Vector next_move = safe_move(response.game_info.field_info.planet_radius, response.game_info.field_info.field_radius, my_location, my_velocity, enemy_location, enemy_velocity, response.game_info.max_turns - response.game_state.current_turn,
                 danger(pos, vel, epos, evel));
         cout << "Next move: " << next_move << endl;
         CommandParams params;
