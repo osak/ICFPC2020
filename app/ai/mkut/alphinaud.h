9866263daa2ddb2634bb588eb711f20e6da774a4
Mirai Towa <mkut.at@gmail.com>
2020-07-20 02:11:57 +0900

    殺意の波動に目覚めしアルフィノ

--- app/ai/mkut/alphinaud.h
+++ app/ai/mkut/alphinaud.h
@@ -189,8 +189,8 @@ public:
     }
     StartParams start_params(const GameResponse& response) {
         int spec_point = response.game_info.ship_info.max_points;
-        int reactor = max(spec_point - 80, 0) / 16;
-        int armament = max(spec_point - 80, 0) / 16;
+        int reactor = 10;
+        int armament = max(spec_point - 200, 0) / 4;
         int engine = spec_point - 2 - reactor * 12 - armament * 4;
         int core = 1;
         return StartParams(engine, armament, reactor, core);
