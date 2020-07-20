6704bbe3f14c9c47c82fa5197843408c82a73642
Osamu Koga <osak.63@gmail.com>
2020-07-20 06:11:15 +0000

    non-ratedもクロールするめう

--- manarimo/manaburn/main.py
+++ manarimo/manaburn/main.py
@@ -4,6 +4,7 @@ from psycopg2.extras import execute_values
 import os
 import time
 from urllib.parse import quote
+from threading import Thread
 
 API_BASE = "https://icfpc2020-api.testkontur.ru"
 API_KEY = "decffdda9f2d431792a37fbfb770f825"
@@ -13,17 +14,17 @@ def get_connection():
     return psycopg2.connect(DATABASE_URL)
 
 def winner_team_name(game):
-    role = game[game['winner'].lower()]
+    role = game.get(game['winner'].lower())
     if role:
         return role['team']['teamName']
     else:
-        '(Draw)'
+        return '(Draw)'
 
 def save(games):
     rows = [
         (
             game['gameId'],
-            game['tournamentId'],
+            game.get('tournamentId', 0),
             game['createdAt'],
             game['ticks'],
             game['winner'],
@@ -92,7 +93,7 @@ def paginated_rated_games(tournament_id: int):
         yield page
 
 def paginated_unrated_games():
-    for page in paginated_games('/games/non-rating'.format(tournament_id)):
+    for page in paginated_games('/games/non-rating'):
         yield page
 
 def save_tournament(tournament_id: int):
@@ -101,17 +102,44 @@ def save_tournament(tournament_id: int):
         save(games)
         time.sleep(0.5)
 
+def save_unrated():
+    for games in paginated_unrated_games():
+        print("saving {} to {}".format(games[0]['createdAt'], games[len(games) - 1]['createdAt']))
+        save(games)
+        time.sleep(0.5)
+
 def backfill():
     for tournament in range(1,5):
         save_tournament(tournament)
+
+class TournamentSaver(Thread):
+    def __init__(self):
+        Thread.__init__(self, name = "TournamentSaver")
+
+    def run(self):
+        while True:
+            save_tournament(5)
+            time.sleep(60)
+
+class UnratedSaver(Thread):
+    def __init__(self):
+        Thread.__init__(self, name = "UnratedSaver")
+
+    def run(self):
+        while True:
+            save_unrated()
+            time.sleep(60)
 
 def run():
     if 'DO_BACKFILL' in os.environ:
         backfill()
 
-    while True:
-        save_tournament(5)
-        time.sleep(60)
-    
+    ts = TournamentSaver()
+    us = UnratedSaver()
+    ts.start()
+    us.start()
+    ts.join()
+    us.join()
+        
 if __name__ == '__main__':
     run()
